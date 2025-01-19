194b13f828860a45b5d76f8457f3b07f95b2c724
MD Ashik Rezwan <ashik.rezwan6@gmail.com>
2025-01-19 23:30:21 +0100

    API & Client side checkout

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51QizgcG8sLropBqPycPmrfC0StYsyysB5u9Ydsyl2N4xNB9n3mN2U53GjKOWDc4V7DP2QvB7kzoqZJL3rlxk3LTM00JZhTqpKQ'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51QizgcG8sLropBqPycPmrfC0StYsyysB5u9Ydsyl2N4xNB9n3mN2U53GjKOWDc4V7DP2QvB7kzoqZJL3rlxk3LTM00JZhTqpKQ'
 };

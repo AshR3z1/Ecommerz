388ca8c5f45d13324a6b6b7f84d9aa8a470263e8
MD Ashik Rezwan <ashik.rezwan6@gmail.com>
2025-01-21 20:10:02 +0100

    API orders

--- Infrastructure/Migrations/20250121171701_OrderAggregateAdded.Designer.cs
+++ Infrastructure/Migrations/20250121171701_OrderAggregateAdded.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20250121171701_OrderAggregateAdded")]
+    partial class OrderAggregateAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -423,10 +426,10 @@ namespace Infrastructure.Migrations
                             b1.Property<int>("ExpMonth")
                                 .HasColumnType("int");
 
-                            b1.Property<int>("ExpYear")
+                            b1.Property<int>("Last4")
                                 .HasColumnType("int");
 
-                            b1.Property<int>("Last4")
+                            b1.Property<int>("Year")
                                 .HasColumnType("int");
 
                             b1.HasKey("OrderId");
@@ -505,7 +508,7 @@ namespace Infrastructure.Migrations
                             b1.Property<int>("ProductId")
                                 .HasColumnType("int");
 
-                            b1.Property<string>("ProductName")
+                            b1.Property<string>("Productname")
                                 .IsRequired()
                                 .HasColumnType("nvarchar(max)");
 
---
+++ Infrastructure/Migrations/20250121182031_PaymentSummaryCorrection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentSummaryCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentSummary_Year",
+                table: "Orders",
+                newName: "PaymentSummary_ExpYear");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemOrdered_Productname",
+                table: "OrderItems",
+                newName: "ItemOrdered_ProductName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentSummary_ExpYear",
+                table: "Orders",
+                newName: "PaymentSummary_Year");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemOrdered_ProductName",
+                table: "OrderItems",
+                newName: "ItemOrdered_Productname");
+        }
+    }
+}

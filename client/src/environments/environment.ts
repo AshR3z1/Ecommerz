0670524fa07392ef6011859b0a1300b4b24fb608
MD Ashik Rezwan <ashik.rezwan6@gmail.com>
2025-01-24 21:39:41 +0100

    Client orders & payment

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51QizgcG8sLropBqPycPmrfC0StYsyysB5u9Ydsyl2N4xNB9n3mN2U53GjKOWDc4V7DP2QvB7kzoqZJL3rlxk3LTM00JZhTqpKQ'
 };
